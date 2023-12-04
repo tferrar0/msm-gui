98b9c485c4e6b1303b969fdcb0cd5d4cf8c2ea73
tferrar0 <146215589+tferrar0@users.noreply.github.com>
2023-12-04 17:40:56 +0000

    caught up to video end

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,23 @@
 class MoviesController < ApplicationController
 
+  def update
+    m_id = params.fetch("the_id")
+    
+    matching_records = Movie.where({ :id => m_id})
+    the_movie = matching_records.at(0)
+
+    the_movie.title = params.fetch("the_title")
+    the_movie.year = params.fetch("the_year")
+    the_movie.duration = params.fetch("the_duration")
+    the_movie.description = params.fetch("the_description")
+    the_movie.image = params.fetch("the_image")
+    the_movie.director_id = params.fetch("the_director_id")
+
+    the_movie.save
+
+    redirect_to("/movies/#{the_movie.id}")
+  end
+
   def destroy
     the_id = params.fetch("an_id")
     matching_records = Movie.where({ :id => the_id })
@@ -9,10 +27,7 @@ class MoviesController < ApplicationController
     redirect_to("/movies")
   end
 
-
   def create
-    #Parameters: {"the_title"=>"1", "the_year"=>"2", "the_duration"=>"3", "the_description"=>"4", "the_image"=>"5", "the_director_id"=>"6"}
-
     m = Movie.new
     m.title = params.fetch("the_title")
     m.year = params.fetch("the_year")
